6170fa7ab1e1b58c36e6c1d4457db50778c2d44d
Jaykishan <86801284+JaykishanJ@users.noreply.github.com>
2021-08-11 00:49:18 -0500

    Delete Retrive_pdbfile.py

--- Retrive_pdbfile.py
+++
@@ -1,7 +0,0 @@
-#before running this command please make sure you have installed Biopython
-from Bio.PDB import *
-pdbl = PDBList()
-print("Please enter only 4 char pdb id")
-structure=input("Enter pdb id: ")#get input from user
-pdbl.retrieve_pdb_file(structure, pdir="E:\pdb", file_format = 'pdb')#make sure you change path according to what you 
-
